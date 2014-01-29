528c3b6d87bcb6c040252e33f73b0f6bb708a8c8
lathel <han12015@byui.edu>
2014-01-29 12:13:05 -0700

    testcommit
    see if ignore works

--- Testappl/gen/com/example/testappl/R.java
+++ Testappl/gen/com/example/testappl/R.java
@@ -38,6 +38,7 @@ public final class R {
         public static final int activity_logonconf=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int badlogon=0x7f030002;
+        public static final int login_confirmation=0x7f030003;
     }
     public static final class menu {
         public static final int logonconf=0x7f070000;

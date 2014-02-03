e1f66eb0bb201ff449112f7dfd85d80c478adf95
Chris Hepworth <hep09002@byui.edu>
2014-02-03 12:04:40 -0700

    Transitions framework
    Adding transitions to the files for switching.

--- Testapp2/src/com/example/testapp2/MainActivity.java
+++ Testapp2/src/com/example/testapp2/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.testapp2;
 
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.transition.Scene;
+import android.transition.TransitionManager;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+@TargetApi(19)
 public class MainActivity extends Activity {
-	
-	public final static String EXTRA_MESSAGE = "This is the messege";
 
+	ViewGroup container;
+	Scene current;
+	Scene other;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		container = (ViewGroup) findViewById(R.id.container);
+		current = Scene.getSceneForLayout(container, R.layout.scene01, this);
+		other = Scene.getSceneForLayout(container, R.layout.logonconfirmation, this);
+		current.enter();
 	}
 
 	@Override
@@ -23,13 +34,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	public void sendlogon(View view) {
-    	EditText editText = (EditText) findViewById(R.id.uNameText);
-    	String message = editText.getText().toString();
-	    setContentView(R.layout.logonconfirmation);
-	    TextView welcome = (TextView) findViewById(R.id.welcome);
-	    welcome.setText("Welcome " + message);
+		EditText editText = (EditText) findViewById(R.id.uNameText);
+		String message = editText.getText().toString();
+		TransitionManager.go(other);
+		TextView welcome = (TextView) findViewById(R.id.welcome);
+		welcome.setText("Welcome " + message);
 	}
 
 }

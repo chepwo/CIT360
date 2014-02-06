be97a3174fdb864604ece0c39f81c11e76b94d23
lathel <han12015@byui.edu>
2014-02-06 12:03:56 -0700

    re-upload/refresh index
    hello

---
+++ Testapp2_server/src/testapp2_server/Server_main.java
@@ -0,0 +1,41 @@
+package testapp2_server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.quickconnectfamily.json.*;
+
+
+public class Server_main {
+
+	public static void main(String[] args) {
+
+		try {
+			//a socket opened on the specified port
+			ServerSocket aListeningSocket = new ServerSocket(6000);
+			while(true){
+				//wait for a connection
+				System.out.println("Waiting for client connection request.");
+				Socket clientSocket = aListeningSocket.accept();
+				//setup the JSON streams for later use.
+				JSONInputStream inFromClient = new JSONInputStream(clientSocket.getInputStream());
+				JSONOutputStream outToClient = new JSONOutputStream(clientSocket.getOutputStream());
+				//read until the client closes 
+				//the connection.
+				while(true){
+					System.out.println("Waiting for a message from the server.");
+					HashMap aMap = (HashMap)inFromClient.readObject();
+					System.out.println("Just got:"+aMap+" from client");
+					CommunicationBean aResponse = new CommunicationBean("Done",(ArrayList)aMap.get("data"));
+					outToClient.writeObject(aResponse);
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}

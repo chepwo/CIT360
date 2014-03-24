30f719de3af90e19674a2ddca4fdb34a43d8ca22
lathel <han12015@byui.edu>
2014-03-24 12:10:32 -0600

    Handlers and streams
    handlers

--- Testapp2/src/com/example/testapp2/MainActivity.java
+++ Testapp2/src/com/example/testapp2/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.testapp2;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import org.quickconnectfamily.json.*;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
@@ -25,43 +29,19 @@ public class MainActivity extends Activity {
 	Scene other;
 	Scene confirm;
 	Scene register;
-	Socket toServer;
-	JSONInputStream inFromServer;
-	JSONOutputStream outToServer;
+	String uNameString;
 
+	Handler handler;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		System.out.println(" --------  debug 1 ------------");
 		setContentView(R.layout.activity_main);
 		container = (ViewGroup) findViewById(R.id.container);
 		current = Scene.getSceneForLayout(container, R.layout.scene01, this);
 		confirm = Scene.getSceneForLayout(container, R.layout.logonconfirmation, this);
 		register = Scene.getSceneForLayout(container, R.layout.registerpage, this);
 		current.enter();
-		System.out.println(" --------  debug 2 ------------");
-		Thread connectThread = new Thread(new Runnable(){
-			@Override
-			public void run() {
-				try{
-					//connect to the server
-					toServer = new Socket("10.0.2.2", 6000);
-					System.out.println("socket created");
-					// setup the JSON streams to be used later.
-					inFromServer = new JSONInputStream(toServer.getInputStream());
-					outToServer = new JSONOutputStream(toServer.getOutputStream()); 
-					System.out.println("JSON streams set up");
-				} catch(Exception e){
-					e.printStackTrace();
-					// responseView.setText("Error: Unable to communicate with server. "+e.getLocalizedMessage());
-				}				
-			}
-		} );
-		System.out.println(" --------  debug 3 ------------");
-		connectThread.start();
-		System.out.println(" --------  debug 4 ------------");
-
-
+		handler = new Handler();
 	}
 
 	@Override
@@ -70,32 +50,64 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	public class LoginRunnable implements Runnable {
 
+		private Handler handler;
+		private UserBean currentUser;
+		private HashMap aMap;
+
+		public LoginRunnable(Handler handler, UserBean currentUser) {
+			this.handler = handler;
+			this.currentUser = currentUser;
+		}
+		public HashMap getMap(){
+			return this.aMap;
+		}
+		@Override
+		public void run() {
+			try{
+				//connect to the server
+				Socket toServer = new Socket("10.0.2.2", 6000);
+				System.out.println("socket created");
+				// setup the JSON streams to be used later.
+				JSONInputStream inFromServer = new JSONInputStream(toServer.getInputStream());
+				JSONOutputStream outToServer = new JSONOutputStream(toServer.getOutputStream()); 
+				System.out.println("JSON streams set up");
+				outToServer.writeObject(currentUser);
+				aMap = (HashMap)inFromServer.readObject();
+				toSceneMain();
+			} catch(Exception e){
+				e.printStackTrace();
+				// responseView.setText("Error: Unable to communicate with server. "+e.getLocalizedMessage());
+			}
+		}
+
+	}
 	public void sendlogon(View view) {
 		EditText uName = (EditText) findViewById(R.id.uNameText);
-		String uNameString = uName.getText().toString();
+		uNameString = uName.getText().toString();
 		EditText password = (EditText) findViewById(R.id.passwordText);
 		String passwordString = password.getText().toString();
 		UserBean currentUser = new UserBean();
 		currentUser.setuName(uNameString);
 		currentUser.setPassword(passwordString);
-		System.out.println(" --------  debug 5 ------------");
-		try {
-			outToServer.writeObject(currentUser);
-		}  
-		catch (JSONException e) {
-			e.printStackTrace();
-			System.out.println(" --------- unable to send ------------");
-		}
+		LoginRunnable run = new LoginRunnable(handler, currentUser);
+		Thread connectThread = new Thread(run);
+		connectThread.start();
+		HashMap aMap = run.getMap();
+		
+	}
+	
+	public void toSceneMain(){
 		TransitionManager.go(confirm);
 		TextView welcome = (TextView) findViewById(R.id.welcome);
 		welcome.setText("Welcome " + uNameString);
 	}
-	
+
 	public void startregister(View view){
 		TransitionManager.go(register);
 	}
-	
+
 	public void sendRegister(View view){
 		EditText uName = (EditText) findViewById(R.id.regUNameText);
 		String uNameString = uName.getText().toString();
@@ -107,13 +119,8 @@ public class MainActivity extends Activity {
 			UserBean currentUser = new UserBean();
 			currentUser.setuName(uNameString);
 			currentUser.setPassword(password1String);
-			try { 
-				outToServer.writeObject(currentUser);
-			} 
-			catch (JSONException e) {
-				e.printStackTrace();
-				System.out.println(" --------- unable to send ------------");
-			}
+			Thread connectThread = new Thread(new LoginRunnable(handler, currentUser));
+			connectThread.start();
 			TransitionManager.go(confirm);
 			TextView welcome = (TextView) findViewById(R.id.welcome);
 			welcome.setText("Welcome " + uNameString);
@@ -124,7 +131,7 @@ public class MainActivity extends Activity {
 			int duration = Toast.LENGTH_SHORT;
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
-			
+
 		}
 	}
 
